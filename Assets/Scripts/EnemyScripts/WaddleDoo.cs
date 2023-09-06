5bf2ec27d968d7d0c62edcc19b1d043d01f8b216
ChoiSeungWon <chltjsdl0119@gwnu.ac.kr>
2023-09-06 14:30:23 +0900

    23-09-06_1
    변경사항 : 몬스터 웨이들 두 스크립트 수정
    
    //변경됨
    - WaddleDoo.cs

--- Assets/Scripts/EnemyScripts/WaddleDoo.cs
+++ Assets/Scripts/EnemyScripts/WaddleDoo.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -15,145 +16,134 @@ public class WaddleDoo : MonoBehaviour
 
     private Rigidbody2D _rigidbody2D;
 
-    private int randomNumber;
+    public GameObject beam;
+
+    private Vector2 movement;
+
+    private int[] randomNumber;
 
     private float currentTime;
 
-    public GameObject beam;
-
     private bool isAttacking;
+
     private enum State
     {
         Move,
+        Jump,
         Charge,
         Attack,
         Dead
     }
 
     private State _state = State.Move;
+
     private void Start()
     {
         _rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
-        
-        InvokeRepeating("RandomNumber", 0f,2f);
+
+        if (_state == State.Move)
+        {
+            InvokeRepeating("RandomNumber", 0f, 1f);
+        }
     }
 
     private void Update()
     {
-        // currentTime += Time.deltaTime;
-        //
-        // if (randomNumber == 4)
-        // {
-        //     _state = State.Charge;
-        // }
+        currentTime += Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (randomNumber[0] == 0)
         {
-            Attack();
+            _state = State.Jump;
         }
-        
-        // switch (_state)
-        // {
-        //     case State.Move:
-        //         Move();
-        //         break;
-        //     case State.Charge:
-        //         Charge();
-        //         break;
-        //     case State.Attack:
-        //         Attack();
-        //         break;
-        //     case State.Dead:
-        //         Dead();
-        //         break;
-        // }
+        else if (randomNumber[1] == 1)
+        {
+            _state = State.Charge;
+        }
+
+        switch (_state)
+        {
+            case State.Move:
+                Move();
+                break;
+            case State.Jump:
+                Jump();
+                break;
+            case State.Charge:
+                Charge();
+                break;
+            case State.Attack:
+                Attack();
+                break;
+            case State.Dead:
+                Dead();
+                break;
+        }
     }
 
     private void Move()
     {
-        Vector2 movement = new Vector2(moveSpeed, _rigidbody2D.velocity.y);
+        movement = new Vector2(moveSpeed, _rigidbody2D.velocity.y);
         _rigidbody2D.velocity = movement;
-        
-        if (randomNumber == 1)
+    }
+
+    private void Jump()
+    {
+        if (!isJumping)
         {
-            if (!isJumping)
-            {
-                isJumping = true;
-                _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jumpPower);
-                Debug.Log("Jump");
-            }
-            else
-            {
-                return;
-            }
+            isJumping = true;
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jumpPower);
+            Debug.Log("Jump");
         }
+        else
+        {
+            return;
+        }
+
+        _state = State.Move;
     }
 
     private void Charge()
     {
         if (currentTime > 1.5)
         {
-            _state = State.Move;
-            
-            currentTime = 0;
-            
             Debug.Log("Charge");
+
+            _state = State.Attack;
+
+            currentTime = 0;
         }
     }
 
     private void Attack()
     {
-        if (!isAttacking)
+        if (!isAttacking && movement.x < 0)
+        {
+            StartCoroutine(LeftBeam());
+        }
+        else if (!isAttacking && movement.x > 0)
         {
             StartCoroutine(RightBeam());
         }
 
+        _state = State.Move;
     }
 
     IEnumerator LeftBeam()
     {
         isAttacking = true;
-        
-        
-        GameObject beamAttack = Instantiate(beam);
 
-        beamAttack.transform.position = transform.position;
-        beamAttack.transform.rotation = Quaternion.Euler(0,0,0);
-        Destroy(beamAttack,beamTime);
+        float angle = 0;
 
-        yield return new WaitForSeconds(beamTime);
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject beamAttack = Instantiate(beam);
+            beamAttack.transform.position = transform.position;
+            beamAttack.transform.rotation = Quaternion.Euler(0, 0, angle);
+            Destroy(beamAttack, beamTime);
 
-        GameObject beamAttack2 = Instantiate(beam);
-
-        beamAttack2.transform.position = transform.position;
-        beamAttack2.transform.rotation = Quaternion.Euler(0,0,22.5f);
-        Destroy(beamAttack2,beamTime);
-
-        yield return new WaitForSeconds(beamTime);
-
-        GameObject beamAttack3 = Instantiate(beam);
-
-        beamAttack3.transform.position = transform.position;
-        beamAttack3.transform.rotation = Quaternion.Euler(0,0,45);
-        Destroy(beamAttack3,beamTime);
-
-        yield return new WaitForSeconds(beamTime);
-
-        GameObject beamAttack4 = Instantiate(beam);
-
-        beamAttack4.transform.position = transform.position;
-        beamAttack4.transform.rotation = Quaternion.Euler(0,0,67.5f);
-        Destroy(beamAttack4,beamTime);
-
-        yield return new WaitForSeconds(beamTime);
-
-        GameObject beamAttack5 = Instantiate(beam);
-
-        beamAttack5.transform.position = transform.position;
-        beamAttack5.transform.rotation = Quaternion.Euler(0,0,90);
-        Destroy(beamAttack5,beamTime);
-
-        yield return new WaitForSeconds(beamTime);
+            angle += 23.5f;
+            yield return new WaitForSeconds(beamTime);
+        }
 
         isAttacking = false;
 
@@ -163,41 +153,19 @@ public class WaddleDoo : MonoBehaviour
     IEnumerator RightBeam()
     {
         isAttacking = true;
-        
-        
-        GameObject beamAttack = Instantiate(beam);
-        beamAttack.transform.position = transform.position;
-        beamAttack.transform.rotation = Quaternion.Euler(0,0,0);
-        Destroy(beamAttack,beamTime);
-        yield return new WaitForSeconds(beamTime);
 
-        GameObject beamAttack2 = Instantiate(beam);
-        beamAttack2.transform.position = transform.position;
-        beamAttack2.transform.rotation = Quaternion.Euler(0,0,335.5f);
-        Destroy(beamAttack2,beamTime);
+        float angle = 360;
 
-        yield return new WaitForSeconds(beamTime);
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject beamAttack = Instantiate(beam);
+            beamAttack.transform.position = transform.position;
+            beamAttack.transform.rotation = Quaternion.Euler(0, 0, angle);
+            Destroy(beamAttack, beamTime);
 
-        GameObject beamAttack3 = Instantiate(beam);
-        beamAttack3.transform.position = transform.position;
-        beamAttack3.transform.rotation = Quaternion.Euler(0, 0, 312);
-        Destroy(beamAttack3,beamTime);
-
-        yield return new WaitForSeconds(beamTime);
-
-        GameObject beamAttack4 = Instantiate(beam);
-        beamAttack4.transform.position = transform.position;
-        beamAttack4.transform.rotation = Quaternion.Euler(0,0,288.5f);
-        Destroy(beamAttack4,beamTime);
-
-        yield return new WaitForSeconds(beamTime);
-
-        GameObject beamAttack5 = Instantiate(beam);
-        beamAttack5.transform.position = transform.position;
-        beamAttack5.transform.rotation = Quaternion.Euler(0,0,265);
-        Destroy(beamAttack5,beamTime);
-
-        yield return new WaitForSeconds(beamTime);
+            angle -= 23.5f;
+            yield return new WaitForSeconds(beamTime);
+        }
 
         isAttacking = false;
 
@@ -211,13 +179,20 @@ public class WaddleDoo : MonoBehaviour
 
     private void RandomNumber()
     {
-        randomNumber = Random.Range(0, 5);
-        
-        Debug.Log("RandomNumber");
+        List<string> GachaList = new List<string>() { "치킨", "탕수육", "햄버거", "피자", "라면" };
+
+        for (int i = 0; i < 3; i++)
+        {
+            int rand = Random.Range(0, GachaList.Count);
+            print(GachaList[rand]);
+            GachaList.RemoveAt(rand);
+        }
     }
     
 
-    private void OnCollisionEnter2D(Collision2D other)
+
+
+private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Kirby")
         {
