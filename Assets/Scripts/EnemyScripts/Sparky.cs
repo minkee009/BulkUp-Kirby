7357e4996399f8c871a87392bb9ae2e4de8fee6e
ChoiSeungWon <chltjsdl0119@gwnu.ac.kr>
2023-09-09 00:05:34 +0900

    23-09-09_1
    변경사항 : 몬스터 스파키 움직임 및 공격 기능 추가
    
    //변경됨
    - Sparky.cs

--- Assets/Scripts/EnemyScripts/Sparky.cs
+++ Assets/Scripts/EnemyScripts/Sparky.cs
@@ -1,35 +1,144 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Sparky : MonoBehaviour
 {
-    public float jumpPower = 5f;
+    [SerializeField] private float jumpDistance = 1.5f;
+    [SerializeField] private float lowJumpPower = 3.5f; 
+    [SerializeField] private float highJumpPower = 5.0f; 
 
     private Transform kirbyTransform;
 
     private Rigidbody2D _rigidbody2D;
-    enum State
-    {
-        Move,
-        Attack,
-        Dead
-    }
 
-    private State _state = State.Move;
+    private bool isJumping = false;
+
+    private Vector2 direction;
+
+    private float jumpRandomNumber;
+    private float attackRandomNumber;
+
+    private bool isMove = false;
+    private bool isAttack = false; 
+
+    public GameObject spark;
     
     // Start is called before the first frame update
     void Start()
     {
-        kirbyTransform = GameObject.Find("Kirby").transform;
-
         _rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("JumpRandomNumber", 0f, 1f);
+        InvokeRepeating("AttackRandomNumber", 2f, 2f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        kirbyTransform = GameObject.FindWithTag("Kirby").transform;
         
+        direction = kirbyTransform.position - transform.position;
+        direction.Normalize();
+        
+        if (direction.x < 0 && !isMove)
+        {
+            LeftMove();
+
+        }
+        else if (direction.x > 0 && !isMove)
+        {
+            RightMove();
+        }
+        if (!isAttack && attackRandomNumber == 1)
+        {
+            attackRandomNumber = 0;
+            isMove = true;
+
+            StartCoroutine(Charge());
+        }
+
+    }
+
+    private void LeftMove()
+    {
+        if (!isJumping)
+        {
+            isJumping = true;
+            if (jumpRandomNumber == 0)
+            {
+                _rigidbody2D.velocity = new Vector2(-jumpDistance, lowJumpPower);
+                Debug.Log("점프");
+            }
+            else if (jumpRandomNumber == 1)
+            {
+                _rigidbody2D.velocity = new Vector2(-jumpDistance, highJumpPower);
+                Debug.Log("점프");
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void RightMove()
+    {
+        if (!isJumping)
+        {
+            isJumping = true;
+            if (jumpRandomNumber == 0)
+            {
+                _rigidbody2D.velocity = new Vector2(jumpDistance, lowJumpPower);
+                Debug.Log("점프");
+            }
+            else if (jumpRandomNumber == 1)
+            {
+                _rigidbody2D.velocity = new Vector2(jumpDistance, highJumpPower);
+                Debug.Log("점프");
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    IEnumerator Charge()
+    {
+        isAttack = true;
+        
+        yield return new WaitForSeconds(1f);
+
+        GameObject generateSpark = Instantiate(spark);
+        generateSpark.transform.position = transform.position;
+        
+        Destroy(generateSpark, 1.5f);
+
+        yield return new WaitForSeconds(1.5f);
+
+        isMove = false;
+        isAttack = false;
+    }
+
+    void JumpRandomNumber()
+    {
+        jumpRandomNumber = Random.Range(0, 2);
+    }
+
+    void AttackRandomNumber()
+    {
+        attackRandomNumber = Random.Range(0, 3);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
     }
 }
